133bf79992fb07b894885e8140678c38c10d7b67
nick anderson <nick@morethq.com>
2026-06-12 05:33:02 +0000

    feat: initial page and hero animations

--- app/components/ShrimpHero.vue
+++ app/components/ShrimpHero.vue
@@ -3,9 +3,9 @@ const config = useAppConfig()
 </script>
 
 <template>
-  <section class="min-h-[calc(100vh-5rem)] flex flex-col justify-center">
-    <blockquote class="font-mono text-4xl text-fg pb-3">{{ config.profile.tagline }}</blockquote>
-    <p class="font-mono text-sm text-fg/60 pb-6">{{ config.profile.subheading }}</p>
-    <a href="#contact" class="font-mono text-sm text-accent hover:text-fg transition-colors">{{ config.profile.cta }}</a>
+  <section class="min-h-[calc(100vh-5rem)] flex flex-col justify-center hero-stagger">
+    <blockquote class="font-mono text-4xl text-fg pb-3 fade-up">{{ config.profile.tagline }}</blockquote>
+    <p class="font-mono text-sm text-fg/60 pb-6 fade-up">{{ config.profile.subheading }}</p>
+    <a href="#contact" class="font-mono text-sm text-accent hover:text-fg transition-colors fade-up">{{ config.profile.cta }}</a>
   </section>
 </template>
